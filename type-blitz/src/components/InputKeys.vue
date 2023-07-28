fff60c8cfd0d1baa58a974150cd45996c81d86ac
Axay Patel <Patel.2788@buckeyemail.osu.edu>
2023-07-28 12:43:54 -0500

    chore: init

---
+++ type-blitz/src/components/InputKeys.vue
@@ -0,0 +1,33 @@
+<script>
+import { ref } from 'vue'
+
+export default {
+  setup() {
+    const input = ref('')
+    const items = ref([])
+    const add = () => {
+      items.value.push(input.value)
+      input.value = ''
+    }
+
+    return { input, items, add }
+  },
+}
+</script>
+
+<template>
+  <div>
+<!--    <p>{{ items }}</p>-->
+    <h3>Keyboard Input</h3>
+    <p>{{ input }}</p>
+    <input type="text" v-model="input" @keyup="add">
+  </div>
+</template>
+
+<style scoped>
+input {
+  width: 100%;
+  height: 50px;
+  font-size: 2rem;
+}
+</style>
