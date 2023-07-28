7ecfe3a854e3c17bce3a6d4519e0fe8154f0f74f
Axay Patel <Patel.2788@buckeyemail.osu.edu>
2023-07-28 16:18:16 -0500

    chore: clean up app.vue

--- type-blitz/src/App.vue
+++ type-blitz/src/App.vue
@@ -1,40 +1,13 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-import Count from "@/components/count.vue";
-import InputKeys from "@/components/InputKeys.vue";
 import KeyboardDisplay from "@/components/KeyboardDisplay.vue";
 </script>
 
 <template>
-<!--  <header>-->
-<!--    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />-->
-
-<!--    <div class="wrapper">-->
-<!--      <HelloWorld msg="You did it!" />-->
-<!--    </div>-->
-<!--  </header>-->
-
-<!--  <main>-->
-<!--    <TheWelcome />-->
-<!--  </main>-->
-<!--  <div class="input">-->
-<!--  <InputKeys />-->
-<!--  </div>-->
-
   <div class="keyboard">
     <KeyboardDisplay />
   </div>
 </template>
 
 <style scoped>
-.input {
-  position: fixed;
-  bottom: 30em;
-  left: 0;
-  right: 0;
-  display: flex;
-  justify-content: center;
-}
 
 </style>
